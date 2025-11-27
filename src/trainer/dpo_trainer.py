0fcc78031c01dc1da3296c61fa3ec76bffafd05b
2U1 <kirito01@naver.com>
2025-11-27 20:15:59 +0900

    -fix: leave the logic to handle videos in DPOTrainer.

--- src/trainer/dpo_trainer.py
+++ src/trainer/dpo_trainer.py
@@ -3,6 +3,7 @@ import torch
 from torch import nn
 from pathlib import Path
 import torch.nn.functional as F
+from typing import Union
 
 from transformers.trainer import (
     is_sagemaker_mp_enabled,
@@ -17,6 +18,7 @@ from transformers.pytorch_utils import (
     ALL_LAYERNORM_LAYERS
 )
 from trl import DPOTrainer
+from trl.trainer.utils import pad_to_length, flush_left, selective_log_softmax
 from train.train_utils import get_peft_state_non_lora_maybe_zero_3
 
 def maybe_zero_3(param, ignore_status=False, name=None):
@@ -37,6 +39,155 @@ class QwenDPOTrainer(DPOTrainer):
 
     def __init__(self, *args, **kwargs):
         super(QwenDPOTrainer, self).__init__(*args, **kwargs)
+
+    def _prepare_dataset(
+        self,
+        dataset,
+        processing_class,
+        args,
+        dataset_name
+    ):
+        return dataset
+
+    @staticmethod
+    def concatenated_inputs(
+        batch: dict[str, Union[list, torch.LongTensor]], padding_value: int
+    ) -> dict[str, torch.LongTensor]:
+
+        concatenated_batch = {}
+
+        concatenated_batch['prompt_input_ids'] = torch.cat([batch["prompt_input_ids"], batch["prompt_input_ids"]], dim=0)
+        concatenated_batch['prompt_attention_mask'] = torch.cat([batch["prompt_attention_mask"], batch["prompt_attention_mask"]], dim=0)
+
+        if 'pixel_values' in batch:
+            concatenated_batch['pixel_values'] = torch.cat([batch["pixel_values"], batch["pixel_values"]], dim=0)
+            concatenated_batch['image_grid_thw'] = torch.cat([batch["image_grid_thw"], batch["image_grid_thw"]], dim=0)
+
+        if 'pixel_values_videos' in batch:
+            concatenated_batch['pixel_values_videos'] = torch.cat(
+                [batch["pixel_values_videos"], batch["pixel_values_videos"]], dim=0
+            )
+            concatenated_batch['video_grid_thw'] = torch.cat(
+                [batch["video_grid_thw"], batch["video_grid_thw"]], dim=0
+            )
+
+        if 'second_grid_ts' in batch:
+            concatenated_batch['second_grid_ts'] = torch.cat(
+                [batch["second_grid_ts"], batch["second_grid_ts"]], dim=0
+            )
+
+        max_completion_length = max(batch["chosen_input_ids"].shape[1], batch["rejected_input_ids"].shape[1])
+
+        concatenated_batch['completion_input_ids'] = torch.cat(
+            (
+                pad_to_length(batch["chosen_input_ids"], max_completion_length, pad_value=padding_value),
+                pad_to_length(batch["rejected_input_ids"], max_completion_length, pad_value=padding_value),
+            ),
+        )
+
+        concatenated_batch['completion_attention_mask'] = torch.cat(
+            (
+                pad_to_length(batch["chosen_attention_mask"], max_completion_length, pad_value=0),
+                pad_to_length(batch["rejected_attention_mask"], max_completion_length, pad_value=0),
+            ),
+        )
+
+        return concatenated_batch
+    
+
+    def concatenated_forward(self, model: nn.Module, batch: dict[str, Union[list, torch.LongTensor]]):
+
+        num_examples = batch['prompt_input_ids'].shape[0]
+        
+        concatenated_batch = self.concatenated_inputs(batch, padding_value=self.padding_value)
+
+        model_kwargs = {}
+
+        if self.aux_loss_enabled:
+            model_kwargs['output_router_logits'] = True
+
+        # Add image/video values to model kwargs
+        if 'pixel_values' in batch:
+            model_kwargs['pixel_values'] = concatenated_batch['pixel_values']
+            model_kwargs['image_grid_thw'] = concatenated_batch['image_grid_thw']
+        if 'pixel_values_videos' in batch:
+            model_kwargs['pixel_values_videos'] = concatenated_batch['pixel_values_videos']
+            model_kwargs['video_grid_thw'] = concatenated_batch['video_grid_thw']
+        if 'second_grid_ts' in batch:
+            model_kwargs['second_grid_ts'] = concatenated_batch['second_grid_ts']
+
+        prompt_input_ids = concatenated_batch["prompt_input_ids"]
+        prompt_attention_mask = concatenated_batch["prompt_attention_mask"]
+        completion_input_ids = concatenated_batch["completion_input_ids"]
+        completion_attention_mask = concatenated_batch["completion_attention_mask"]
+        
+        input_ids = torch.cat((prompt_input_ids, completion_input_ids), dim=1)
+        attention_mask = torch.cat((prompt_attention_mask, completion_attention_mask), dim=1)
+        loss_mask = torch.cat(
+            (torch.zeros_like(prompt_attention_mask), completion_attention_mask), dim=1
+        )
+
+        # Flush left to reduce the memory usage
+        # [[0, 0, x, x, x, x],  ->  [[x, x, x, x],
+        #  [0, x, x, x, 0, 0]]       [x, x, x, 0]]
+        attention_mask, input_ids, loss_mask = flush_left(attention_mask, input_ids, loss_mask)
+
+        model_kwargs["attention_mask"] = attention_mask
+
+        outputs = model(input_ids, **model_kwargs)
+        logits = outputs.logits
+
+        labels = torch.roll(input_ids, shifts=-1, dims=1)
+        loss_mask = torch.roll(loss_mask, shifts=-1, dims=1).bool()
+
+        if logits.shape[:2] != labels.shape[:2]:
+            # for llava, the returned logits include the image tokens (placed before the text tokens)
+            seq_len = labels.shape[1]
+            logits = logits[:, -seq_len:]
+
+        # Compute the log probabilities of the labels
+        labels[~loss_mask] = 0  # dummy token; we'll ignore the losses on these tokens later
+        per_token_logps = selective_log_softmax(logits, labels)
+        per_token_logps[~loss_mask] = 0
+        per_token_logps = torch.roll(per_token_logps, shifts=1, dims=1)
+
+        all_logps = per_token_logps.sum(-1)
+
+        output = {}
+
+        if self.use_weighting:
+            with torch.no_grad():
+                # Eq (2) of the WPO paper: https://huggingface.co/papers/2406.11827
+                logprobs = F.log_softmax(logits, dim=-1)
+                weights_adjustment_factor = torch.logsumexp(2 * logprobs, dim=-1)  # same as sum(probs**2) in log space
+                per_token_logps_adjusted = per_token_logps - weights_adjustment_factor
+                all_weights = (per_token_logps_adjusted * loss_mask).sum(-1) / loss_mask.sum(-1)
+                chosen_weights = all_weights[:num_examples]
+                rejected_weights = all_weights[num_examples:]
+                output["policy_weights"] = torch.clamp(torch.exp(chosen_weights + rejected_weights), max=1)
+
+        if self.args.rpo_alpha is not None:
+            # Only use the chosen logits for the RPO loss
+            chosen_logits = logits[:num_examples]
+            chosen_labels = labels[:num_examples]
+
+            # Compute the log probabilities of the labels
+            output["nll_loss"] = F.cross_entropy(
+                torch.flatten(chosen_logits, end_dim=1), torch.flatten(chosen_labels, end_dim=1), ignore_index=0
+            )
+
+        if self.loss_type == "ipo":
+            all_logps = all_logps / loss_mask.sum(-1)
+
+        output["chosen_logps"] = all_logps[:num_examples]
+        output["rejected_logps"] = all_logps[num_examples:]
+        output["mean_chosen_logits"] = logits[:num_examples][loss_mask[:num_examples]].mean()
+        output["mean_rejected_logits"] = logits[num_examples:][loss_mask[num_examples:]].mean()
+
+        if self.aux_loss_enabled:
+            output["aux_loss"] = outputs.aux_loss
+
+        return output
 
     def create_optimizer(self):
         """
